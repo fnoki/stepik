52ef3b48c1c564128821fb709612f9430aee5634
k1pq <fnokbivalez@gmail.com>
2023-04-13 21:10:50 +0300

    Update def_sum_range.py

--- code/some_task/def_sum_range.py
+++ code/some_task/def_sum_range.py
@@ -1,8 +1,8 @@
 start, end = int(input("начало: ")), int(input("конец: "))
 
 
-def sum_range(start: int, end: int) -> str:
-    return f"Сумма в диапазоне равняется: {sum(range(start, end + 1))}"
+def sum_range(_start: int, _end: int) -> str:
+    return f"Сумма в диапазоне равняется: {sum(range(_start, _end + 1))}"
 
 
 print(sum_range(start, end))
